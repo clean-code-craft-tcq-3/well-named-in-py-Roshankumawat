c241a53fe28fd69ee795e7470b0ffbb86c1285cf
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-04-10 11:53:15 +0530

    Updated Main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import ColorPair
 
 def testNumberToPair(pair_number,
                         expected_major_color, expected_minor_color):
-  major_color, minor_color = getColorFromPairNumber(pair_number)
+  major_color, minor_color = getColorFromPairNumber(pair_number) #noqa: F821
   assert major_color == expected_major_color, 'Detected Major color did not match with Expected Major color'
   assert minor_color == expected_minor_color,'Detected Major color did not match with Expected Major color'
 
 def testPairToNumber(major_color, minor_color, expected_pair_number):
-  pair_number = getPairNumberFromColor(major_color, minor_color)
+  pair_number = getPairNumberFromColor(major_color, minor_color)#noqa: F821
   assert pair_number == expected_pair_number, 'Detected Major color did not match with Expected Major color'
 
 if __name__ == '__main__':
