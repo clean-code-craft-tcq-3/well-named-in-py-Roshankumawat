498b49662fb909f8ca10b9c5edd7799dbab2abab
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-04-10 18:33:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import ColorPair
 
-def testNumberToPair(pair_number,
-                        expected_major_color, expected_minor_color):
-  major_color, minor_color = getColorFromPairNumber(pair_number) # noqa: F821
-  assert major_color == expected_major_color, 'Detected Major color did not match with Expected Major color'
-  assert minor_color == expected_minor_color,'Detected Major color did not match with Expected Major color'
+def testNumberToPair():
+   def __init__(self, PairNumber, ExpMajorColor, ExpMinorColor):
+      self.PairNumber= PairNumber
+      self.ExpMajorColor= ExpMajorColor
+      self.ExpMinorColor= ExpMinorColor
+      
+      major_color, minor_color = getColorFromPairNumber(PairNumber) # noqa: F821
+      assert major_color == ExpMajorColor, 'Detected Major color did not match with Expected Major color'
+      assert minor_color == ExpMinorColor,'Detected Major color did not match with Expected Major color'
 
-def testPairToNumber(major_color, minor_color, expected_pair_number):
-  pair_number = getPairNumberFromColor(major_color, minor_color) # noqa: F821
-  assert pair_number == expected_pair_number, 'Detected Major color did not match with Expected Major color'
+def testPairToNumber():
+    def __init__(self,major_color, minor_color, expected_pair_number):
+      pair_number = getPairNumberFromColor(major_color, minor_color) # noqa: F821
+      assert pair_number == expected_pair_number, 'Detected Major color did not match with Expected Major color'
 
 if __name__ == '__main__':
   testNumberToPair(4, 'White', 'Red')
