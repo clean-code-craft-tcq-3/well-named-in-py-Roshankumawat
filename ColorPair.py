1ad27802d29000cc651b1b267e2f8b250c581944
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-04-21 10:17:42 +0530

    Update ColorPair.py
    Corrected as per review request

--- ColorPair.py
+++ ColorPair.py
@@ -4,7 +4,7 @@ MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
 def getColorFromPairNumber(pair_number):
   zero_based_pair_number = pair_number - 1
-  major_index = zero_based_pair_number // len(MAJOR_COLORS)
+  major_index = zero_based_pair_number // len(MINOR_COLORS)
   if major_index >= len(MAJOR_COLORS):
     raise Exception('Major index out of range')
   minor_index = zero_based_pair_number % len(MINOR_COLORS)
